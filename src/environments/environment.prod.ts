764d13c9c46122b874fe9ff9c2b7a4487c5a2d78
malav <malav@github.com>
2018-04-02 16:10:57 -0400

    Add Firebase to the App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+
+    apiKey: "AIzaSyCgUvvtApJw_r5nURdJPRjU9QihyyLIP3A",
+    authDomain: "recomweb-6ac77.firebaseapp.com",
+    databaseURL: "https://recomweb-6ac77.firebaseio.com",
+    projectId: "recomweb-6ac77",
+    storageBucket: "recomweb-6ac77.appspot.com",
+    messagingSenderId: "537989273888"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,17 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyCgUvvtApJw_r5nURdJPRjU9QihyyLIP3A",
+    authDomain: "recomweb-6ac77.firebaseapp.com",
+    databaseURL: "https://recomweb-6ac77.firebaseio.com",
+    projectId: "recomweb-6ac77",
+    storageBucket: "recomweb-6ac77.appspot.com",
+    messagingSenderId: "537989273888"
+
+  }
+
+
+  
 };
