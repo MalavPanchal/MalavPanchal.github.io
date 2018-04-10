827cd7fc54c19452746a88778ffa0aa2ddc8c05f
malav <malav@github.com>
2018-04-10 17:03:53 -0400

    show and hide Admin links

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 import { CanActivate } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
-import 'rxjs/add/operator/switchMap';
+
 import 'rxjs/add/operator/map';
 @Injectable()
 export class AdminAuthGuard implements CanActivate{
@@ -12,8 +12,7 @@ export class AdminAuthGuard implements CanActivate{
   constructor(private auth: AuthService, private userService: UserService) { }
 
 canActivate(): Observable<boolean>{
-return this.auth.user$
-.switchMap(user =>  this.userService.get(user.uid))
+return this.auth.appUser$
 .map(appUser => appUser.isAdmin);
 
 
--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { AppUser } from '../models/app-user';
 
 
 @Component({
@@ -8,16 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent  {
+appUser: AppUser;
 
 
-
-  constructor(public auth: AuthService ) {
-
+  constructor(private auth: AuthService ) {
+auth.appUser$.subscribe(appUser => this.appUser = appUser); 
    }
 
-  ngOnInit() {
-  }
-
+  
 
   logout(){
 this.auth.logout();
