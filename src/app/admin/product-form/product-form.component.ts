2413843aa115519564cae045947c117fc193025a
malav <malav@github.com>
2018-04-13 16:30:11 -0400

    implement the routes for 'edit a product'

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,22 +1,27 @@
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/take';
 
 @Component({
-  selector: 'app-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.css']
+    selector: 'app-product-form',
+    templateUrl: './product-form.component.html',
+    styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
-categories$;
+    categories$;
+    product = {};
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     private CategoryService: CategoryService,
     private productService: ProductService) {
     this.categories$ = CategoryService.getCategories();
 
+    let id = this.route.snapshot.paramMap.get('id');
+    if (id) this.productService.get(id).take(1).subscribe(p => this.product = p);
    }
 
    save(product){
