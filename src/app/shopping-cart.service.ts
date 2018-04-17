3645300d3d3a696a53c7c760724b50750c64cb01
malav <malav@github.com>
2018-04-17 18:19:54 -0400

    refactor the AddToCart method

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,15 +1,52 @@
+import { async } from '@angular/core/testing';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
+import { Product } from './models/product';
+import 'rxjs/add/operator/take';
 
 @Injectable()
 export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create(){
+  private create(){
    return  this.db.list('shopping-carts ').push({
       dataCreated: new Date().getTime()
     });
   }
+
+  private getCart(cartId: string){
+    return this.db.object('/shopping-carts/ ' + cartId);
+  }
+
+private async getOrCreateCartId(){
+  let cartId = localStorage.getItem('cartId');
+  if (cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key );
+    return result.key;
+
+  
+
+  
+  
+
+}
+
+private getItem(cartId: string, productId: string){
+  return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+
+}
+
+
+
+async addToCart(product : Product){
+  let cartId = await this.getOrCreateCartId();
+  let item$ = this.getItem(cartId, product.$key);
+  item$.take(1).subscribe(item => {
+   item$.update({product: product,quantity: (item.quantity || 0) + 1});
+  });
+}
 
 }
