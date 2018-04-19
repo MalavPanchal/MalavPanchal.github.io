cdc34e2c5f053906fc5aad2bbc8001384d64e445
malav <malav@github.com>
2018-04-19 18:18:48 -0400

    Add Order summary card and Refactor
    Refactoring: Extract shipping form Component

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,45 +1,25 @@
-import { Router } from '@angular/router';
-import { AuthService } from './../auth.service';
-import { OrderService } from './../order.service';
-import { Subscription } from 'rxjs/Subscription';
+
 import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from './../shopping-cart.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Order } from './../models/order';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit, OnDestroy { 
-  shipping = {}; 
-  cart:ShoppingCart;
-  cartSubscription: Subscription;
-  userId: string;
-  userSubscription: Subscription;
+export class CheckOutComponent implements OnInit { 
+  cart$: Observable<ShoppingCart>;
+ 
 
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private orderService: OrderService,
-    private shoppingCartServive: ShoppingCartService ){}
+  constructor(private shoppingCartServive: ShoppingCartService ){}
 
   async ngOnInit(){
-    let cart$ = await this.shoppingCartServive.getCart();
-    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
-    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
+    this.cart$ = await this.shoppingCartServive.getCart();
+   
   }
-  ngOnDestroy(){
-  this.cartSubscription.unsubscribe();
-  this.userSubscription.unsubscribe();
-  }
-
-  async placeOrder() {
-
-let order = new Order(this.userId, this.shipping, this.cart)
-    let result = await this.orderService.placeOrder(order);
-    this.router.navigate(['/order-success', result.key]);
-  } 
   
+
  
 }
